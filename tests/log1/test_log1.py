09ce6f93cc9c1941ed4b6219dbb97dc43e0df4b9
Fritz Mahnke <fritz@mahnke.tech>
2025-08-26 08:51:33 -0700

    test(log1): add tests for log_args

--- tests/log1/test_log1.py
+++ tests/log1/test_log1.py
@@ -155,3 +155,46 @@ class TestLog:
             assert match is not None
             assert match.group(2) == 'INFO'
             assert match.group(3) == 'log message'
+
+
+@log1.log_args(level=log1.DEBUG)
+def log_args_fn(arg_0: str, arg_1: int) -> None:
+    _ = arg_0
+    _ = arg_1
+
+
+class TestLogArgs:
+    def test_enabled(self, log_file: Path) -> None:
+        log1.init(
+            'd',
+            stream=False,
+            log_file_path=log_file,
+            log_file_mode='w',
+            color=False
+        )
+
+        log_args_fn('arg_0 value', 1234)
+
+        with open(log_file) as file:
+            data = file.readlines()
+
+            assert len(data) == 1
+
+            assert data[
+                0] == "DEBUG: log_args_fn(arg_0='arg_0 value', arg_1=1234)\n"
+
+    def test_disabled(self, log_file: Path) -> None:
+        log1.init(
+            'i',
+            stream=False,
+            log_file_path=log_file,
+            log_file_mode='w',
+            color=False
+        )
+
+        log_args_fn('arg_0 value', 1234)
+
+        with open(log_file) as file:
+            data = file.readlines()
+
+            assert len(data) == 0
